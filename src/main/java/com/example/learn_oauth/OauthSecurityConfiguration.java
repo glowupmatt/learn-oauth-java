dc3b61efc3483c068ce9df58a9c427ff4e6f3714
Matthew Nicholson <thematthewnicholson@gmail.com>
2024-11-16 10:13:10 -0800

    learn Oauth

---
+++ src/main/java/com/example/learn_oauth/OauthSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.learn_oauth;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class OauthSecurityConfiguration {
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .anyRequest().authenticated()
+            )
+            .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
